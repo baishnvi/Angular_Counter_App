4d0ffa45aeaccdd421f9a27da3131182e57c0603
baishnvi <qweengoswami81@gmail.com>
2026-05-27 11:54:13 +0530

    angular Build simple counter app UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { ProfileComponent } from '../profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login,Signup,ProfileComponent,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tut');
+  count = 0
+  handleCounter(val:string){
+    if(val =='minus'){
+      this.count = this.count - 1
+    }else if(val == 'plus'){
+      this.count = this.count+1
+    }
+    else{
+      this.count=0
+    }
+  }
 }
+
