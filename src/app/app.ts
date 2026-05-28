42ef885ad01ef73732857d03fb5e9bee27adac11
baishnvi <qweengoswami81@gmail.com>
2026-05-28 15:58:24 +0530

    Implemented Angular events with get and set input value

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,16 +11,32 @@ import { ProfileComponent } from '../profile/profile';
   styleUrl: './app.css'
 })
 export class App {
-  count = 0
-  handleCounter(val:string){
-    if(val =='minus'){
-      this.count = this.count - 1
-    }else if(val == 'plus'){
-      this.count = this.count+1
-    }
-    else{
-      this.count=0
-    }
-  }
-}
 
+ handleEvent(event: any) {
+  console.log('Event Type:', event.type);
+
+  // Popup notification
+  alert(`Event Triggered: ${event.type}`);
+
+  }
+
+  name=""
+  displayName="";
+  email="";
+  getName(event:Event){
+    this.name=(event.target as HTMLInputElement).value
+  }
+  showName(){
+    this.displayName=this.name;
+  }
+  setName(){
+    this.name="Baishnvi"
+  }
+   getEmail(val:string){
+    console.log(val);
+    this.email=val;
+   }
+   setEmail(){
+    this.email="default@test.com"
+   }
+}
